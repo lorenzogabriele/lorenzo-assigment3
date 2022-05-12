ab0ff9cce9873c229c7bafc42e79cd64d464265d
DESKTOP-IFQMLGE\PC <lorenzo.gabriele40@gmail.com>
2022-05-11 20:53:46 -0500

    v1.2

--- src/com/bootcamp/FileService.java
+++ src/com/bootcamp/FileService.java
@@ -1,9 +1,47 @@
 package com.bootcamp;
 
-public class FileService {
-	
-	//find out about the user array in the user buffer reading
-	
-	
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class FileService {
+
+	{
+		BufferedReader lorenzoReader = null;
+		String line = "";
+		
+		// find out about the user array in the user buffer reading
+		try {
+			while (line != null) {
+				try {
+					lorenzoReader = new BufferedReader(new FileReader("data.txt"));
+					line = lorenzoReader.readLine();
+				} catch (FileNotFoundException e) {
+					System.out.println("Oh shoot file not found exception!");
+					e.printStackTrace();
+				
+				} catch (IOException e) {
+					System.out.println("Ops there was a IO Exception!");
+					e.printStackTrace();
+				}
+				
+				System.out.println();
+				}
+				
+				} finally {
+					
+					try {
+						System.out.println("Closing lorenzoReader");
+						lorenzoReader.close();
+						
+					} catch (IOException e) {
+						e.printStackTrace();
+
+					}
+					
+				}
+					
+				}
+		
 }
